570afb29bfe1cc5773a8babb552e84728d03dd60
David Hérault <dherault@gmail.com>
2020-07-04 22:15:53 +0200

    Create BinaryChart

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -1,17 +1,44 @@
 import React from 'react'
 import './App.css'
 
+import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 
+import BinaryChart from './components/BinaryChart'
+
 function App() {
+  const data = createFakeData()
+
+  console.log('data', data)
+
   return (
     <Container maxWidth="md">
       <Typography variant="h3" component="h1">
         Visualizer
       </Typography>
+      <Box mt={4}>
+        <BinaryChart data={data} />
+      </Box>
     </Container>
   )
+}
+
+function createFakeData(nWords = 512) {
+  const nBits = 32
+  const words = []
+
+  for (let i = 0; i < nWords; i++) {
+    let word = ''
+
+    for (let j = 0; j < nBits; j++) {
+      word += Math.random() > 0.5 ? 1 : 0
+    }
+
+    words.push(parseInt(word, 2).toString(16))
+  }
+
+  return words
 }
 
 export default App
---
+++ visualizer/src/components/BinaryChart.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const clockWidth = 24
+const strokeWidth = 2
+const clockHeight = clockWidth - strokeWidth
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingBottom: theme.spacing(1),
+    overflowX: 'scroll',
+    display: 'flex',
+  },
+  stroke: {
+    height: strokeWidth,
+    width: clockWidth,
+    backgroundColor: theme.palette.primary.main,
+  },
+  ceil: {
+    height: clockHeight,
+    width: strokeWidth,
+  },
+}))
+
+function BinaryChart({ data }) {
+  const _ = useStyles()
+
+  const binaryData = data
+    .map(hex => `0${parseInt(hex, 16).toString(2)}`.split(''))
+    .flatMap(x => x)
+
+  return (
+    <div
+      className={_.root}
+    >
+      {binaryData.map(b => b === '1' ? (
+        <div>
+          <div className={_.stroke} />
+          <div className={_.ceil} />
+        </div>
+      ) : (
+        <div>
+          <div className={_.ceil} />
+          <div className={_.stroke} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BinaryChart
