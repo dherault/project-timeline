d2896078a3ba89531558887bdf3f154b46bc034b
David Hérault <dherault@gmail.com>
2020-07-04 18:00:57 +0200

    Add visualizer
    To visualize io data

---
+++ visualizer/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h3" component="h1">
+        Visualizer
+      </Typography>
+    </Container>
+  )
+}
+
+export default App
---
+++ visualizer/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import './index.css'
+
+import theme from './theme'
+
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
