ef5d75f70d8b2ade776d2a6860e7bda39667c9e2
David Hérault <dherault@gmail.com>
2020-07-07 14:09:45 +0200

    wip

--- python-test/TSL2591.test.py
+++ python-test/TSL2591.test.py
@@ -28,8 +28,8 @@ sensor = adafruit_tsl2591.TSL2591(i2c)
 # Read the total lux, IR, and visible light levels and print it every second.
 while True:
     # Read and calculate the light level in lux.
-    lux = sensor.lux
-    print("Total light: {0}lux".format(lux))
+    # lux = sensor.lux
+    # print("Total light: {0}lux".format(lux))
     # You can also read the raw infrared and visible light levels.
     # These are unsigned, the higher the number the more light of that type.
     # There are no units like lux.
@@ -37,9 +37,9 @@ while True:
     infrared = sensor.infrared
     print("Infrared light: {0}".format(infrared))
     # Visible-only levels range from 0-2147483647 (32-bit)
-    visible = sensor.visible
-    print("Visible light: {0}".format(visible))
+    # visible = sensor.visible
+    # print("Visible light: {0}".format(visible))
     # Full spectrum (visible + IR) also range from 0-2147483647 (32-bit)
-    full_spectrum = sensor.full_spectrum
-    print("Full spectrum (IR + visible) light: {0}".format(full_spectrum))
+    # full_spectrum = sensor.full_spectrum
+    # print("Full spectrum (IR + visible) light: {0}".format(full_spectrum))
     # time.sleep(1.0)
